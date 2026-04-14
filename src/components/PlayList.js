df88d0acebd597f96f50f45ea3862899b1f14aee
Michal Gruda <michalgru@gmail.com>
2026-04-14 13:19:52 +0200

    Added styles

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -6,12 +6,12 @@ const PlayList = props => {
   const { id, title, pictureSmall, tracklistUrl, noOfTracks, url = tracklistUrl ?? `playlist/${id}/tracks` } = props;
 
   return (
-    <>
+    <div className='playlist-card'>
       <img className='pictureSmall' src={pictureSmall} alt={title} />
       <div className='playlistName'>{title}</div>
       <div className='playlistNoOfSongs'>{noOfTracks}</div>
       <Tracks url={url} />
-    </>
+    </div>
   );
 };
 export default PlayList;
